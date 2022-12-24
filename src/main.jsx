457a988a5b682b6105ec1718172c8630568790a8
Jordan Mannfeld <jordanmannfeld@gmail.com>
2022-12-24 09:49:55 -0500

    adds react-snowfall

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Snowfall from "react-snowfall";
 import Home from "./pages/Home";
 import MerryChristmas from "./pages/MerryChristmas";
 import "./index.css";
@@ -18,6 +19,12 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Snowfall
+      color="#e2e2e2"
+      snowflakeCount={75}
+      radius={[1.0, 4.0]}
+      wind={[0.25, 1]}
+    />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
