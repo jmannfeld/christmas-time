c6975ebb8ae727edd5d7f823d58ea7d4d52885bf
Jordan Mannfeld <jordanmannfeld@gmail.com>
2022-12-25 10:45:37 -0500

    links to gift card

--- src/pages/MerryChristmas.jsx
+++ src/pages/MerryChristmas.jsx
@@ -4,10 +4,13 @@ import "./MerryChristmas.css";
 function MerryChristmas() {
   return (
     <div className="christmas-wrapper bg">
-      <h1 className="christmas-heading">Merry Christmas, Cory! 🎄</h1>
+      <h1 className="christmas-heading">
+        Merry Christmas, Chuck and Ronda! 🎄
+      </h1>
       <a
-        href="https://beginnerjavascript.com/"
+        href="/vitos-gift-card.pdf"
         target="_blank"
+        type="application/pdf"
         className="button"
       >
         Open your Christmas gift 🎁
